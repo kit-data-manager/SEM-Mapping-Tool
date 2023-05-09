e43805ce4cf8296d9b8232e5694150e84121bb69
Elias Vitali <elias@Eliass-MacBook-Air.fritz.box>
2023-05-09 14:51:45 +0200

    debugging

--- main/envTest.py
+++ main/envTest.py
@@ -1,1 +1,8 @@
-from dateutil import parser
+try:
+    from dateutil import parser
+except:
+    print("dateutil not found")
+try:
+    import numpy as np
+except:
+    print("numpy not found, dateutil not found")
